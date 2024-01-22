e3652928a0cfacd190d3fc9753a0b31f66df1a25
ujawall_ <fullstackctp@gmail.com>
2024-01-22 18:01:24 +0530

    PasswordEncoder and UserDetailService UserDetailsManager

--- springsecuritybasics2/src/main/java/com/securitybasics2/springsecuritybasics/sonfig/ProjectSecurityConfig.java
+++ springsecuritybasics2/src/main/java/com/securitybasics2/springsecuritybasics/sonfig/ProjectSecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.SneakyThrows;
@@ -35,4 +40,43 @@ public class ProjectSecurityConfig {
 				
 		return http.build();
 	}
-}
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		
+		//By using defaultPasswordEncoder
+//		UserDetails admin = User.withDefaultPasswordEncoder()
+//				.username("admin")
+//				.password("1234")
+//				.authorities("admin")
+//				.build();
+//		
+//		UserDetails user = User.withDefaultPasswordEncoder()
+//				.username("user")
+//				.password("12345")
+//				.authorities("read")
+//				.build();
+		
+		
+		
+		//By using NoOpPasswordEncoder()
+		
+		UserDetails admin = User.withUsername("admin")
+				.password("1234")
+				.authorities("admin")
+				.build();
+		
+		UserDetails user = User.withUsername("user")
+				.password("12345")
+				.authorities("read")
+				.build();
+		
+		return new InMemoryUserDetailsManager(admin,user);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	}
