fb2950884c04f9a99687b109db9e162bd17c6b7e
Tarun Kumar <tarunkumar04223@gmail.com>
2024-02-14 11:42:15 +0530

    Implemented method level security

--- EasyBank/src/main/java/com/springsecurity/eazybank/EazyBankApplication.java
+++ EasyBank/src/main/java/com/springsecurity/eazybank/EazyBankApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true )
 @SpringBootApplication
-@EnableMethodSecurity(prePostEnabled = true)
 public class EazyBankApplication {
 
     public static void main(String[] args) {
